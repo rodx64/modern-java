40eca164f594ee10d567841a333f883d8c1b1b8d
Rodrigo <rodrigo.bertini@gmail.com>
2023-06-26 20:11:35 -0300

    testing BiFunction ...

--- src/main/java/rba/modernjava/functionalProgramming/function/BiFunctionExample.java
+++ src/main/java/rba/modernjava/functionalProgramming/function/BiFunctionExample.java
@@ -6,11 +6,21 @@ import rba.modernjava.functionalProgramming.Instructors;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.BiFunction;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class BiFunctionExample {
 
     public static void main(String[] args) {
+        System.out.println("Function with Hashmap: ");
+        functionTestOne();
+
+        System.out.println("\nBiFunction with Predicate");
+        functionTestWithBiFunction();
+    }
+
+    private static void functionTestOne() {
         Function<List<Instructor>, Map<String, Integer>> mapFunction = (instructors) -> {
             Map<String, Integer> map = new HashMap<>();
             instructors.forEach(instructor -> {
@@ -20,9 +30,26 @@ public class BiFunctionExample {
         };
 
         System.out.println(mapFunction.apply(Instructors.getAll()));
-
     }
 
+    private static void functionTestWithBiFunction() {
+        Predicate<Instructor> instructorPredicate = p -> p.isOnlineCourse() == true;
+
+        BiFunction<List<Instructor>, Predicate<Instructor>, Map<String, Integer>> mapBiFunction =
+            (instructors, predicate) -> {
+                Map<String, Integer> map = new HashMap<>();
+
+                instructors.forEach(instructor -> {
+                    if(instructorPredicate.test(instructor)){
+                        map.put(instructor.getName(), instructor.getYearsOfExp());
+                    }
+                });
+
+            return map;
+        };
+
+        System.out.println(mapBiFunction.apply(Instructors.getAll(), instructorPredicate));
+    }
 
 
 }
